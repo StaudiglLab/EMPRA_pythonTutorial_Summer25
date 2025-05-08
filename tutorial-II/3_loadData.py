6e2c293b50acafcc97ba80ce5713d303eb31b7c7
chowdhuryaditya <chowdhury@ncra.tifr.res.in>
2025-05-08 17:24:37 +0200

    tutorial-II codes

---
+++ tutorial-II/3_loadData.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+#loading only first three columns
+ts,left_x,left_y=np.loadtxt('exampleEyetrackerData.tsv',usecols=[0,1,2],unpack=True,skiprows=1)
+
+#TODO in class. Show how to look at the size of the array and how to retrieve elements in the array (contraints on what indexes can be)
